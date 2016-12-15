b7b14ac9ac2012d940c391e97d253e84246add4a
Snowkylin Lazarus <Snowkylin Lazarus>
2016-12-16 00:21:30 +0800

    bug fixed

--- image_denoising.py
+++ image_denoising.py
@@ -42,18 +42,18 @@ x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 
-print x_train.shape # [60000, 784]
-print x_test.shape  # [10000, 784]
+print x_train.shape # [60000, 1, 28, 28]
+print x_test.shape  # [10000, 1, 28, 28]
 
 autoencoder.fit(x_train_noisy, x_train, nb_epoch = epoch, validation_data=(x_test_noisy, x_test))
 
-decoded_imgs = autoencoder.predict(x_test)
+decoded_imgs = autoencoder.predict(x_test_noisy)
 n = 10
 for i in range(n):
     plt.subplot(2, n, i + 1)
     # subplot(nrows, ncols, plot_number)
     # http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.subplot
-    plt.imshow(x_test[i].reshape(image_width, image_width))
+    plt.imshow(x_test_noisy[i].reshape(image_width, image_width))
 
     plt.subplot(2, n, n + i + 1)
     plt.imshow(decoded_imgs[i].reshape(image_width, image_width))
